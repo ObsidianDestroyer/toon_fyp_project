d930094b836304fab90737f676f2fb0b80da7e2b
Alexander Walther | ObsidianDestroyer <asushofman@gmail.com>
2022-04-10 02:33:29 +0300

    [MS] Added base "RecorderPage" cls

---
+++ lib/views/recorder_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+
+class RecorderPage extends StatefulWidget {
+  const RecorderPage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  State<RecorderPage> createState() => _RecorderPageState();
+}
+
+
+class _RecorderPageState extends State<RecorderPage> {
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
